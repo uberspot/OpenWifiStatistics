30df857bcdf8824c489c8aba7e5a0f9e074c8dbc
Pavlos <onexemailx@gmail.com>
2011-12-07 22:32:26 +0200

    made first transition

--- src/com/ews/EasyWifiStatistics/StartPage.java
+++ src/com/ews/EasyWifiStatistics/StartPage.java
@@ -1,13 +1,21 @@
 package com.ews.EasyWifiStatistics;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartPage extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.startpage);
     }
+    
+    public void goToMeasureConnection(View v) {
+    	Intent i = new Intent(this, MeasureConnectionPage.class);
+        startActivity(i);
+      }
+
 }
