517471f00067fefbb549c936829f2adcf4622dc0
Pavlos <onexemailx@gmail.com>
2011-12-07 02:02:02 +0200

    made start screen

--- gen/com/ews/EasyWifiStatistics/R.java
+++ gen/com/ews/EasyWifiStatistics/R.java
@@ -13,10 +13,23 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int ewsIcon=0x7f050001;
+        public static final int measureConnection=0x7f050004;
+        public static final int scrollView1=0x7f050000;
+        public static final int settings=0x7f050005;
+        public static final int showStatistics=0x7f050003;
+        public static final int toggleMonitoring=0x7f050002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int measure_connection=0x7f040004;
+        public static final int settings=0x7f040005;
+        public static final int show_statistics=0x7f040003;
+        public static final int start_monitoring=0x7f040001;
+        public static final int stop_monitoring=0x7f040002;
     }
 }
