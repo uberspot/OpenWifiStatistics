6d3684e2b174d3ce6a55b96a341cae2f12843a7d
Periklis Ntanasis <pntanasis@gmail.com>
2012-01-29 06:28:36 +0200

    add zoom buttons

--- src/com/ews/EasyWifiStatistics/WifiMapPage.java
+++ src/com/ews/EasyWifiStatistics/WifiMapPage.java
@@ -3,13 +3,19 @@ package com.ews.EasyWifiStatistics;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView; 
 
 public class WifiMapPage extends MapActivity {
+	
+	MapView mapView;
 	
 	 @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wifimappage);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
 	
 	@Override
