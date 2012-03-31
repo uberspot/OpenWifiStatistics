570abcde3d6190591e222c5b39e5a0f8895368e2
Pavlos <onexemailx@gmail.com>
2012-03-31 22:03:14 +0300

    removed forgotten classes

--- src/com/ows/OpenWifiStatistics/Globals.java
+++
@@ -1,9 +0,0 @@
-package com.ows.OpenWifiStatistics;
-
-import com.ows.OpenWifiStatistics.Services.MonitoringService;
-
-public class Globals {
-	
-	public static MonitoringService service = null;
-	
-}
