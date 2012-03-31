62b20a69c270858c3ccee7d12280b7ff1897761b
Pavlos <onexemailx@gmail.com>
2012-03-31 14:56:08 +0300

    removed webinterface part

--- webinterface/configuration.php
+++
@@ -1,7 +0,0 @@
-<?php
-$GLOBALS['host'] = "localhost";
-$GLOBALS['user'] = "le_user";
-$GLOBALS['password'] = "le pass";
-$GLOBALS['database'] = "le_dbname";
-$GLOBALS['googlemapapikey'] = "le_apikey";
-?>
